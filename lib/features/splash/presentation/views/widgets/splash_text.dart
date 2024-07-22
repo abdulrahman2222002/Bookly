38d46427b2a428869d914629d43c89680817ccd9
abdulrahman2222002 <zizoa6098@gmail.com>
2024-07-22 18:15:23 +0300

    Add animation to splash text

---
+++ lib/features/splash/presentation/views/widgets/splash_text.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+class SplashText extends StatelessWidget {
+  const SplashText({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (context, _) {
+        return SlideTransition(
+          position: slidingAnimation,
+          child: const Text(
+            'Read Free Books',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
