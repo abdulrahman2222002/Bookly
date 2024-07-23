144d3e5f674200091afb6caaa67162a78e4f20ab
abdulrahman2222002 <zizoa6098@gmail.com>
2024-07-23 13:11:06 +0300

    add list view item to home

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -2,5 +2,6 @@
 class AssetsData{
 
   static const logo ='assets/images/logo.png';
+  static const testImage ='assets/images/test_image.png';
 
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,8 @@
-
-
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -11,9 +12,32 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-
+        ListViewItem(),
       ],
     );
   }
 }
 
+class ListViewItem extends StatelessWidget {
+  const ListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .25,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(14),
+            image: const DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
